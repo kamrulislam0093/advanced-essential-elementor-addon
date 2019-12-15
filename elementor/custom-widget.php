15b343dd61dd449b270b42d466c20daa431ed74d
kamrul <mdkamrulislam093@gmail.com>
2019-12-15 13:30:18 +0600

    include custom-widget.php three widget

--- elementor/custom-widget.php
+++ elementor/custom-widget.php
@@ -14,9 +14,8 @@ add_action( 'elementor/elements/categories_registered', function ( $elements_man
 
 
 require_once ACF_DIR_PATH . 'elementor/element/comparision-product.php';
-
-// require_once ACF_DIR_PATH . 'elementor/element/st-slider.php';
-// require_once ACF_DIR_PATH . 'elementor/element/st-recent-product.php';
-// require_once ACF_DIR_PATH . 'elementor/element/st-category-product.php';
+require_once ACF_DIR_PATH . 'elementor/element/st-slider.php';
+require_once ACF_DIR_PATH . 'elementor/element/st-recent-product.php';
+require_once ACF_DIR_PATH . 'elementor/element/st-category-product.php';
 
 
